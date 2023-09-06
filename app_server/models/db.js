897bdec254d2d5eacf41cd6439fcca356e98d659
483ashritha <ashrithasadhu@gmail.com>
2023-09-06 11:11:20 +0530

    First commit

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,14 +1,23 @@
-const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost/LOCATOR';
+var mongoose=require('mongoose');
+var loc=require('./locations')
+require('dot').config();
+const PORT=process.env.PORT || 3000;
+
+var gracefulshutdown;
+const dbURI='mongodb://0.0.0.0:27017/loc8r';
+if(process.env.NODE_ENV === 'production'){
+   dbURI=process.env.MONGODB_URI;
+}
 mongoose.connect(dbURI);
 
-mongoose.connection.on('connected', () => {
-  console.log(`Mongoose connected to ${dbURI}`);
+mongoose.connection.on('connected',function () {
+  console.log('Mongoose connected to' +dbURI);
+
 });
-mongoose.connection.on('error', err => {
-  console.log('Mongoose connection error:', err);
+mongoose.connection.on('error',function(err){
+  console.log('Mongoose connection error:' +err);
 });
-mongoose.connection.on('disconnected', () => {
+mongoose.connection.on('disconnected',function (){
   console.log('Mongoose disconnected');
 });
 require('./locations');
